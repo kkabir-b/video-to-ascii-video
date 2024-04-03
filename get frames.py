385722da37c1c92c679bca39bc13c9a9f9b5834e
kkabir-b <107753204+kkabir-b@users.noreply.github.com>
2024-04-03 23:49:56 +0800

    Update get frames.py

--- get frames.py
+++ get frames.py
@@ -1,8 +1,8 @@
 import cv2
 vid = cv2.VideoCapture('bad_apple.mp4')
 success,image = vid.read()
-c = 0
+counter= 0
 while success:
-  cv2.imwrite("frame%d.jpg" % c, image)    # save frame as JPEG file
+  cv2.imwrite("frame%d.jpg" % counter, image)    # save frame as JPEG file
   success,image = vid.read()
-  c += 1
+  counter += 1
